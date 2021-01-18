ec4b399a550f01a168923e8fb7e5113362274d36
LogosInc <duomaxwell7032@gmail.com>
2021-01-18 23:12:35 +0800

    Designed Bottom Sheet

---
+++ app/src/main/java/com/example/foody/UI/fragments/recipes/RecipesFragment.kt
@@ -0,0 +1,132 @@
+package com.example.foody.UI.fragments.recipes
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foody.viewmodels.MainViewModel
+import com.example.foody.R
+import com.example.foody.adpaters.RecipesAdapter
+import com.example.foody.databinding.FragmentRecipesBinding
+import com.example.foody.util.NetworkResult
+import com.example.foody.util.observeOnce
+import com.example.foody.viewmodels.RecipesViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class RecipesFragment : Fragment() {
+
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var recipesViewModel: RecipesViewModel
+
+    private val mAdapter by lazy {RecipesAdapter()}
+    // Deleted after add _binding and binding
+    //private lateinit var mView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+
+        //view.recylerview.showShimmer()
+
+        //recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+
+        setupRecyclerView()
+        readDatabase()
+
+        /**  */
+        //requestApiData()
+
+        return binding.root
+    }
+
+    private fun setupRecyclerView(){
+        binding.recylerview.adapter = mAdapter
+        binding.recylerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun readDatabase() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observeOnce(viewLifecycleOwner, { database ->
+            if (database.isNotEmpty()){
+                Log.d("RecipesFragment", "readDatabase called!")
+                mAdapter.setData(database[0].foodRecipe)
+                hideShimmerEffect()
+            }else{
+                requestApiData()
+            }
+        }) }
+
+    }
+
+    private fun requestApiData(){
+        Log.d("RecipesFragment", "requestApiData called!")
+        mainViewModel.getRecipes(recipesViewModel.applyQueries())
+        mainViewModel.recipesResponse.observe(viewLifecycleOwner, { response ->
+            when(response){
+                is NetworkResult.Success -> {
+                    hideShimmerEffect()
+                    response.data?.let { mAdapter.setData(it) }
+                }
+                is NetworkResult.Error -> {
+                    hideShimmerEffect()
+                    loadDataFromCache()
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    showShimmerEffect()
+                }
+            }
+        })
+    }
+
+    private fun loadDataFromCache(){
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observe(viewLifecycleOwner, {database ->
+            if (database.isNotEmpty()){
+                mAdapter.setData(database[0].foodRecipe)
+            }
+        }) }
+
+    }
+
+    private fun showShimmerEffect(){
+        binding.recylerview.showShimmer()
+    }
+
+    private fun hideShimmerEffect() {
+        binding.recylerview.hideShimmer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -0,0 +1,22 @@
+package com.example.foody.util
+
+class Constants {
+
+    companion object{
+
+        const val BASE_URL ="https://api.spoonacular.com"
+        const val API_KEY = "6c6567d4dc9543ba9d98f208f80da83b"
+
+        // API Query Keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+        // ROOM DATABASE
+        const val DATABASE_NAME ="recipe_database"
+        const val RECIPES_TABLE = "recipes_table"
+    }
+}
